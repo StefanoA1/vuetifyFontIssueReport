d4d2c405892b1fc4d76cbcc285bbfcd63288c59b
stefano.acosta <stefano.acostabermeo@margo-group.com>
2018-11-26 17:35:29 +0100

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <h1>This should be Montserrat, which it isn't: aaaa gggg</h1>
+    <h3>This should be Montserrat, which it isn't: aaaa gggg</h3>
+    <br>
+    <p>This should be Open Sans, which it is: aaaa gggg</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
